dc64c4e7c54c82b5dddda8a9de88844a14fc85d0
Nikolas Lopes <nikolaslopes20@gmail.com>
2024-09-22 23:59:51 -0300

    KRYOS SSAM

---
+++ script.js
@@ -0,0 +1,122 @@
+window.revelar=ScrollReveal({reset:true})
+
+// PAGINA 1
+
+revelar.reveal('.js-efeito-pag-titulo',{
+    duration: 500, 
+    distance: '90px',
+    origin: 'bottom'
+})
+
+revelar.reveal('.js-efeito-texto',{
+    duration: 500, 
+    distance: '90px',
+    origin: 'left',
+    delay: 500
+})
+
+revelar.reveal('.js-efeito-img',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'top',
+    delay: 1000
+})
+
+// PAGINA 2
+
+revelar.reveal('.js-efeito-texto1',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'left',
+    delay: 800
+})
+
+revelar.reveal('.js-efeito-texto2',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'left',
+    delay: 900
+})
+
+revelar.reveal('.js-efeito-texto3',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'left',
+    delay: 1000
+})
+
+revelar.reveal('.js-efeito-texto4',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'left',
+    delay: 1100
+})
+
+revelar.reveal('.js-efeito-texto5',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'left',
+    delay: 1200
+})
+
+revelar.reveal('.js-efeito-texto6',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'left',
+    delay: 1300
+})
+
+revelar.reveal('.js-efeito-texto6',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'left',
+    delay: 1400
+})
+
+revelar.reveal('.js-efeito-img-2',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'right',
+    delay: 1500
+})
+
+// PAGINA 4
+
+revelar.reveal('.js-efeito-dow1',{
+    duration: 500, 
+    distance: '90px',
+    origin: 'bottom',
+    delay: 500
+})
+
+revelar.reveal('.js-efeito-dow2',{
+    duration: 500, 
+    distance: '90px',
+    origin: 'bottom',
+    delay: 600
+})
+
+revelar.reveal('.js-efeito-dow3',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'bottom',
+    delay: 700
+})
+
+revelar.reveal('.js-efeito-dow4',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'bottom',
+    delay: 800
+})
+
+
+revelar.reveal('.js-efeito-itens',{
+    duration: 500, 
+    distance: '100px',
+    origin: 'left',
+    delay: 900
+})
+
+
+
